738651600a1e357168a98e9dea10525fe365d1f2
Nour kayyali <nourkayyali91@gmail.com>
2023-10-13 18:05:17 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -2,7 +2,23 @@ const example = [
   ["A", "A", "A"],
   ["A", "A", "A"],
   ["A", "B", "A"]];
-
+console.log(example.length);
 const whereIsWaldo = (matrix) => {
-  
+  let waldo = matrix[0][0];
+  for (let i=0;i<matrix.length;i++)
+  {
+    for (let j=0;j<matrix[i].length;j++)
+    {
+      //const nestedEl1=matrix[i][j];
+      if (waldo !== matrix[i][j])
+    {
+      
+      console.log ([i+1,j+1]);
+      return [i+1,j+1];
+      return (matrix[i+1],matrix[i+1][j+1]);
+    } }
+
+  }
+  return null;
 }
+whereIsWaldo(example);
